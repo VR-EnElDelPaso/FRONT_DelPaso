2bf961e3ebd190c2abd8e74a3750fcde0769178f
Branyoe <branyoe.dev@gmail.com>
2024-07-11 14:27:56 -0600

    feat: add auth options to appBar

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,10 @@
-import { useCallback, useEffect} from "react";
+import { useCallback, useEffect } from "react";
 import useAuthStore from "../stores/AuthStore";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 
   const {
-    isAuthenticated,
     setIsAuthenticated,
-    user,
     setUser
   } = useAuthStore();
 
@@ -31,24 +29,8 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     checkAuthStatus();
   }, [checkAuthStatus])
 
-  const handleLogin = () => {
-    // posible manera de redirección, requiere configuración en el servidor
-    // window.location.href = 'http://localhost:4006/api/auth/login?redirect_uri=http://localhost:3000/auth/callback';
-    window.location.href = 'http://localhost:4006/api/auth/login';
-  }
-
-  const handleLogout = async () => {
-    window.location.href = 'http://localhost:4006/api/auth/logout';
-  }
-
   return (
     <>
-      {user && <p>{user.uCorreo}</p>}
-      {
-        isAuthenticated
-          ? (<button onClick={handleLogout}>Logout</button>)
-          : (<button onClick={handleLogin}>Login</button>)
-      }
       {children}
     </>
   )
