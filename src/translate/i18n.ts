4fd085796accf8e8c998defb0e80ff08a4e8390c
RKamey <ralonso@ucol.mx>
2024-07-28 16:11:51 -0600

    feat: Update language selection logic to use saved language from local storage #25

--- src/translate/i18n.ts
+++ src/translate/i18n.ts
@@ -4,6 +4,8 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { es } from './translations_es';
 import { en } from './translations_en';
 
+const savedLanguage = localStorage.getItem('i18nextLng');
+
 i18n
     .use(LanguageDetector)
     .use(initReactI18next)
@@ -13,10 +15,14 @@ i18n
             en: en,
         },
         fallbackLng: 'es',
-        lng: 'es',
+        lng: savedLanguage || 'es',
         interpolation: {
             escapeValue: false,
-        }
+        },
+        detection: {
+            order: ['localStorage', 'navigator'],
+            lookupLocalStorage: 'i18nextLng',
+        },
     })
 
 export default i18n;
