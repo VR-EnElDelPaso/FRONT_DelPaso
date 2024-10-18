00ac698ad191570e681a9af1399c9b69c869e027
RKamey <ralonso@ucol.mx>
2024-10-18 10:09:41 -0600

    chore: update AppBar and NavLink styles

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -24,8 +24,10 @@ export default function AppBar() {
     <nav className="sticky top-0 z-50 bg-white shadow-md">
       {/* Animación del AppBar */}
       <ZoomInOnScroll duration={2} initialScale={.95}>
+        
         {/* Contenido */}
         <div className="h-20 p-4 container mx-auto flex justify-between items-center">
+            
             {/* Logos */}
             <div className='h-full'>
               <a href="/" className="h-full flex cursor-pointer items-center gap-4 transition-transform hover:scale-105">
@@ -34,37 +36,43 @@ export default function AppBar() {
                 <img className='h-full object-contain' src={Udc} alt="Logo UDC" />
               </a>
             </div>
+
             {/* Acciones */}
             <div className="hidden lg:flex space-x-5">
+              
               {/* Menú */}
-              <div className="font-bold hidden lg:flex space-x-20">
+              <div className="flex gap-10 items-center">
                 <NavLink href="#">{t('Home')}</NavLink>
                 <NavLink href="#">{t('About Muvi')}</NavLink>
                 <NavLink href="#">{t('Help')}</NavLink>
               </div>
               
-              <CartButton />
-              <LanguageSelector />
-              {
-                isAuthenticated ? (<>
-                  <div className='flex justify-center items-center gap-1'>
-                    <FaUser className="text-xl" />
-                    <h4>{user?.display_name}</h4>
-                  </div>
-                  <button title='sign out' onClick={handleLogout} className="bg-black bg-opacity-75 text-white px-4 py-1 rounded-md transition duration-200 hover:bg-opacity-100 font-normal">
-                    <FaSignOutAlt className="mr-1" />
-                  </button>
-                </>) : (<>
-                  <button onClick={handleLogin} className="bg-black bg-opacity-75 text-white px-4 py-1 rounded-md transition duration-200 hover:bg-opacity-100 font-normal">
-                    {t('Login')}
-                  </button>
-                </>)
-              }
+              {/* Botones acciones */}
+              <div className="flex gap-5">
+                <CartButton />
+                <LanguageSelector />
+                {
+                  isAuthenticated ? (<>
+                    <div className='flex justify-center items-center gap-1'>
+                      <FaUser className="text-xl" />
+                      <h4>{user?.display_name}</h4>
+                    </div>
+                    <button title='sign out' onClick={handleLogout} className="bg-black bg-opacity-75 text-white px-4 py-1 rounded-xl transition duration-200 hover:bg-opacity-100 font-normal">
+                      <FaSignOutAlt className="mr-1" />
+                    </button>
+                  </>) : (<>
+                    <button onClick={handleLogin} className="bg-primary bg-opacity-75 text-white px-4 py-1 rounded-xl transition duration-200 hover:bg-opacity-100 font-normal">
+                      {t('Login')}
+                    </button>
+                  </>)
+                }
+              </div>
             </div>
             <div className="lg:hidden flex items-center">
                 <MenuButton isOpen={menuOpen} onClick={toggleMenu} />
             </div>
         </div>
+        {/* Menú en vista mobile */}
         {menuOpen && (
           <div className=" font-bold lg:hidden mt-4 flex flex-col space-y-12 items-center">
             <NavLink href="#">{t('Home')}</NavLink>
