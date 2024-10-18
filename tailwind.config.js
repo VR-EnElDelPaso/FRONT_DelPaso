6884e55accad5038408b4e19faeecb712fdc7036
Luis Pinto <lpinto1@ucol.mx>
2024-10-17 18:13:05 -0600

    chore: add dark textColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        kaiseiDecol: ['Kaisei Decol', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
+        kaiseiDecol: ["Kaisei Decol", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
       backgroundColor: {
-        secondary: '#5F5EA3',
-        primary: '#B33424',
-        primaryHover: '#A52A1A',
+        secondary: "#5F5EA3",
+        primary: "#B33424",
+        primaryHover: "#A52A1A",
       },
       borderColor: {
-        primary: '#B33424',
+        primary: "#B33424",
       },
       textColor: {
-        primary: '#B33424',
-        secondary: '#5F5EA3',
+        primary: "#B33424",
+        secondary: "#5F5EA3",
+        dark: "#333333",
       },
     },
   },
