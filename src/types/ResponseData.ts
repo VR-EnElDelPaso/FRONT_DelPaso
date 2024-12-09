0c3b7f98f2c2956ef4fa96bf99eb9de8fdc0aa4a
Luis Pinto <lpinto1@ucol.mx>
2024-12-09 06:14:13 -0600

    chore: Add pagination type on response data

--- src/types/ResponseData.ts
+++ src/types/ResponseData.ts
@@ -1,6 +1,12 @@
 export default interface ResponseData {
-    ok: boolean;
-    message: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data?: any;
+  ok: boolean;
+  message: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data?: any;
+  pagination: {
+    total: number;
+    page: number;
+    limit: number;
+    hasMore: boolean;
+  };
 }
