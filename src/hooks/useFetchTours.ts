f3f3262f98235a0bcda44d5c1a42e99cb5d57aa2
Luis Pinto <lpinto1@ucol.mx>
2025-01-01 21:14:12 -0600

    chore: update useFetchTours.ts

--- src/hooks/useFetchTours.ts
+++ src/hooks/useFetchTours.ts
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 
-import { getAllTours } from '../services/Tour';
+import { getAllTours } from "../services/Tour";
+
+import { Tour } from "@/types/tour"; // Assuming you have a Tour type defined
 
 const useFetchTours = () => {
-  const [tours, setTours] = useState([]);
+  const [tours, setTours] = useState<Tour[]>([]);
 
   useEffect(() => {
-    getAllTours().then((tours) => {
-      console.log(tours);
-      setTours(tours);
-    })
+    getAllTours().then((response) => {
+      console.log(response);
+      setTours(response.data);
+    });
   }, []);
 
   return tours;
-}
-
+};
 
 export default useFetchTours;
