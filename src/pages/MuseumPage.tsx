60b2224745e5e1e1c677b77dbdd8a81045bc1ca5
RKamey <ralonso@ucol.mx>
2025-02-07 14:41:58 -0600

    feat: improve MuseumStatus component and integrate it into MuseumPage

--- src/pages/MuseumPage.tsx
+++ src/pages/MuseumPage.tsx
@@ -1,24 +1,26 @@
 import { Card } from "@/components/ui/card";
 import { getMuseumById } from "@/services/Museums";
 import { Museum } from "@/types/Museums";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Clock, MapPin, DollarSign } from "lucide-react";
 import Carousel from "@/components/NowShowing/Carousel";
 import { Button } from "@/components/ui/button";
+import MuseumStatus from "@/components/NowShowing/MuseumStatus";
 
 const MuseumPage = () => {
   const { id } = useParams<{ id: string }>();
   const [museum, setMuseum] = useState<Museum | null>(null);
 
-  useEffect(() => {
-    const fetchMuseum = async () => {
-      const museum = await getMuseumById(id as string);
-      setMuseum(museum);
-    };
-
-    fetchMuseum();
+  const fetchMuseum = useCallback(async () => {
+    const museum = await getMuseumById(id as string);
+    console.log(museum);
+    setMuseum(museum);
   }, [id]);
+
+  useEffect(() => {
+    fetchMuseum();
+  }, [fetchMuseum]);
 
   if (!museum) return null;
 
@@ -77,17 +79,21 @@ const MuseumPage = () => {
               <div className="rounded-full bg-primary p-3 mb-3">
                 <DollarSign className="h-6 w-6 text-primary-foreground" />
               </div>
-              <h3 className="font-semibold mb-2">Precio</h3>
+              <h3 className="font-semibold mb-2">Cuota de Recuperación</h3>
               <p className="text-sm text-muted-foreground text-center">
-                 Precio no disponible
+                Cuota no disponible
               </p>
             </div>
+          </div>
+
+          <div className="flex justify-end mt-4">
+            <MuseumStatus />
           </div>
 
           {/* Recorridos */}
           <div className="container mx-auto flex flex-col px-4 sm:px-6 md:px-8">
             {/* Title */}
-            <div className="mb-6 sm:mb-8">
+            <div className="mt-4 mb-6 sm:mb-8">
               <div className="flex text-primary/50 items-center mb-2">
                 <span className="text-lg sm:text-xl mr-2">•</span>
                 <p className="text-xs sm:text-sm font-bold tracking-widest uppercase">
@@ -101,7 +107,7 @@ const MuseumPage = () => {
 
             {/* Carousel */}
             <div className="w-full mb-8 sm:mb-10">
-              <Carousel />
+              <Carousel museum_id={museum.id} />
             </div>
 
             {/* Button */}
@@ -134,28 +140,35 @@ const MuseumPage = () => {
 
             {/* Sección de Ayuda */}
             <div className="container mx-auto px-4 py-16">
-              <div className="max-w-3xl mx-auto text-center space-y-6">
-                <div className="flex items-center justify-center gap-2">
-                  <div className="w-2 h-2 rounded-full bg-primary" />
-                  <h3 className="text-sm font-medium text-muted-foreground">
+              <div className="max-w-3xl mx-auto space-y-6">
+                <div className="flex items-center gap-2">
+                  <div className="w-1 h-1 rounded-full bg-primary" />
+                  <h3 className="text-sm font-medium tracking-widest text-primary uppercase text-muted-foreground">
                     Ayuda
                   </h3>
                 </div>
 
-                <h2 className="text-3xl font-kaiseiDecol font-bold">¿Tienes preguntas?</h2>
+                <h2 className="text-3xl font-kaiseiDecol font-bold">
+                  ¿Tienes preguntas?
+                </h2>
 
                 <p className="text-base leading-7 text-muted-foreground">
                   Si tienes alguna duda sobre nuestras exposiciones, horarios,
                   entradas u otros servicios, por favor visita nuestra sección
                   de preguntas frecuentes o contáctanos directamente a través de
                   contacto@muvi.com o llamando al +52 333 123 4567. Estamos aquí
-                  para ayudarte a disfrutar de tu visita al Museo Fernando del
-                  Paso.
+                  para ayudarte a disfrutar de tu visita al {museum.name}.
                 </p>
 
-                <Button variant="default" size="lg" className="font-medium text-white">
-                  Ayuda
-                </Button>
+                <div className="flex justify-end">
+                  <Button
+                    variant="default"
+                    size="lg"
+                    className="font-medium text-white rounded-xl"
+                    >
+                    Ayuda
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
