97c98321f2823ae099f526a276d2dfda7624e295
Luis Pinto <lpinto1@ucol.mx>
2025-02-14 13:32:25 -0600

    feat: add MuseumHours type

--- src/types/Museums.ts
+++ src/types/Museums.ts
@@ -8,3 +8,10 @@ export interface Museum {
   created_at: string;
   updated_at: string;
 }
+
+export interface MuseumHours {
+  day: string;
+  isOpen: boolean;
+  openTime?: string;
+  closeTime?: string;
+}
