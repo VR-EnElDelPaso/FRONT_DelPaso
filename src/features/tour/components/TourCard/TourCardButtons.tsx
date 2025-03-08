1047b9af91ae48d0cf1e29301da69880c58cb936
Branyoe <branyoe.dev@gmail.com>
2025-03-08 13:22:27 -0600

    feat: add confirmation alert for tour navigation and refactor TourCardButtons component

--- src/features/tour/components/TourCard/TourCardButtons.tsx
+++ src/features/tour/components/TourCard/TourCardButtons.tsx
@@ -2,13 +2,26 @@ import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCheckPurchasedTour } from "@/querys/tour.querys";
 import { useCartStore } from "@/stores/useCartStore";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ConfirmAlert } from "./SeeTourAlert";
 
 export const TourCardButtons = ({ tourId }: { tourId: string }) => {
+  // ----[ State ]----
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
   // ----[ Hooks ]----
   const navigate = useNavigate();
   const { setCartItem } = useCartStore();
-  const { data: purchasedResponse, isPending: isPurchasedPending } = useCheckPurchasedTour(tourId);
+  const { data: purchasedResponse, isPending: isPurchasedPending } =
+    useCheckPurchasedTour(tourId);
+
+  // ----[ Handlers ]----
+  const handleCloseAlert = () => setIsAlertOpen(false);
+  const handleConfirmAlert = () => {
+    handleCloseAlert();
+    navigate(`/tours/view/${tourId}`);
+  };
 
   // ----[ Render ]----
   if (isPurchasedPending) {
@@ -19,7 +32,7 @@ export const TourCardButtons = ({ tourId }: { tourId: string }) => {
           <Skeleton className="w-full h-12" />
         </div>
       </>
-    )
+    );
   } else if (purchasedResponse?.data?.purchased === true) {
     return (
       <>
@@ -27,13 +40,24 @@ export const TourCardButtons = ({ tourId }: { tourId: string }) => {
           <Button
             variant="default"
             className="w-full h-12 text-white shadow-none rounded-xl md:w-auto"
-            onClick={() => navigate(`/tours/view/${purchasedResponse?.data?.tour_id}`)}
+            onClick={() => setIsAlertOpen(true)}
           >
             Ver tour
           </Button>
         </div>
+        <ConfirmAlert
+          isOpen={isAlertOpen}
+          onClose={handleCloseAlert}
+          onConfirm={handleConfirmAlert}
+          onCancel={handleCloseAlert}
+          title="¿Estás seguro?"
+          description='Al hacer clic en "Continuar", iniciarás el tour y solo tendrás
+            acceso a él por 24 horas.'
+          confirmText="Continuar"
+          cancelText="Cancelar"
+        />
       </>
-    )
+    );
   } else {
     return (
       <>
@@ -51,12 +75,14 @@ export const TourCardButtons = ({ tourId }: { tourId: string }) => {
           <Button
             variant="outline"
             className="w-full h-12 shadow-none rounded-xl text-dark md:w-auto"
-            onClick={() => setCartItem({ id: tourId, isSelected: true, quantity: 1 })}
+            onClick={() =>
+              setCartItem({ id: tourId, isSelected: true, quantity: 1 })
+            }
           >
             Agregar al carrito
           </Button>
         </div>
       </>
-    )
+    );
   }
-}
+};
