b43969d38356ffdef5963beaac0ad76829878fb7
Luis Pinto <lpinto1@ucol.mx>
2025-05-12 15:21:01 -0600

    fix(admin): delete unused pages

--- src/pages/admin/AdminStats.tsx
+++
@@ -1,9 +0,0 @@
-const AdminStatistics = () => {
-  return (
-    <div>
-      <h1>Admin Estadísticas</h1>
-    </div>
-  )
-}
-
-export default AdminStatistics
--- src/pages/admin/AdminUsers.tsx
+++
@@ -1,9 +0,0 @@
-const AdminUsers = () => {
-  return (
-    <div>
-      <h1>Administrar Usuarios</h1>
-    </div>
-  )
-}
-
-export default AdminUsers
