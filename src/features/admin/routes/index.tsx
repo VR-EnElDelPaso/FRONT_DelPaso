85f2e3b819cf61c95c74a0ead853414bb2d346f2
Luis Pinto <lpinto1@ucol.mx>
2025-05-12 15:20:10 -0600

    fix(admin): delete unused routes

--- src/features/admin/routes/index.tsx
+++ src/features/admin/routes/index.tsx
@@ -1,30 +1,26 @@
 import AdminHome from "@/pages/admin/AdminHome";
 import AdminMuseums from "@/pages/admin/AdminMuseums";
-import AdminStatistics from "@/pages/admin/AdminStats";
 import AdminTours from "@/pages/admin/AdminTours";
-import AdminUsers from "@/pages/admin/AdminUsers";
 import LandingPage from "@/pages/admin/LandingPage";
 import AdminFaqs from "@/pages/admin/AdminFaqs";
 
 const adminRoutes = {
   children: [
     { path: "", element: <AdminHome /> },
-    { path: "users", element: <AdminUsers /> },
     {
       path: "museums",
       element: <AdminMuseums />,
     },
-    { path: "stats", element: <AdminStatistics /> },
     {
       path: "tours",
       element: <AdminTours />,
     },
     {
-      path: "/admin/landing",
+      path: "landing",
       element: <LandingPage />,
     },
     {
-      path: "/admin/faqs",
+      path: "faqs",
       element: <AdminFaqs />,
     },
   ],
