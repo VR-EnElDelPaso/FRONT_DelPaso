2584a543848bcc834b3e8fea773ad12f47cad1c5
Luis Pinto <lpinto1@ucol.mx>
2025-05-12 17:02:19 -0600

    chore(SocialMediaIcons): delete instagram icon

--- src/components/SocialMediaIcons/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons/SocialMediaIcons.tsx
@@ -1,6 +1,6 @@
 import { ZoomInOnScroll } from '../animations/ZoomInOnScroll';
 import SocialMediaIcon from './SocialMediaIcon';
-import { FaFacebook, FaWhatsapp, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaWhatsapp } from 'react-icons/fa';
 
 export default function SocialMediaIcons({ containerClass = "", iconClass = "" }) {
   return (
@@ -13,11 +13,6 @@ export default function SocialMediaIcons({ containerClass = "", iconClass = "" }
       <ZoomInOnScroll duration={.8} delay={.3}>
         <SocialMediaIcon href="https://www.whatsapp.com" ariaLabel="Whatsapp">
           <FaWhatsapp className={iconClass} />
-        </SocialMediaIcon>
-      </ZoomInOnScroll>
-      <ZoomInOnScroll duration={.8} delay={.6}>
-        <SocialMediaIcon href="https://www.instagram.com" ariaLabel="Instagram">
-          <FaInstagram className={iconClass} />
         </SocialMediaIcon>
       </ZoomInOnScroll>
     </div>
