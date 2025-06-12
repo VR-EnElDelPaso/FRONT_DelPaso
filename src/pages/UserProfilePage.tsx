93ee51d038d2a6011a72a09cd838ac9d58ce6e98
Luis Pinto <lpinto1@ucol.mx>
2025-06-12 00:56:29 -0600

    styles: set py of root div

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -182,7 +182,7 @@ export default function UserProfilePage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-gray-50 py-4">
       <div className="container mx-auto px-4 max-w-2xl">
         <Card className="bg-white shadow-sm">
           <CardContent className="p-8">
